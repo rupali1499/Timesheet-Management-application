10a4c7f5787c8fc2e2c3d159da1a59e606926084
Rupali Thakur <trupali96@gmail.com>
2026-06-21 16:52:19 +0530

    fix(*): made login screen responsive

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import LoginForm from "@/components/auth/loginForm";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex items-center">
-      <div className="w-1/2 h-full px-[72px]">
+    <div className="h-screen w-screen flex flex-col md:flex-row items-center">
+      <div className="w-full md:w-1/2 h-full px-6 md:px-[72px] py-8 md:py-0">
         <LoginForm />
       </div>
-      <div className="w-1/2 h-full">
+      <div className="w-full md:w-1/2 h-full hidden md:block">
         <AppDescription />
       </div>
     </div>
